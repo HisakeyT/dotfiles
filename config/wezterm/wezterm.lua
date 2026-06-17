aa6641d1b86493901861a863521f66224b47773e
hisakeyT <110322156+HisakeyT@users.noreply.github.com>
2026-06-18 04:57:24 +0900

    fix: wezterm.lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,9 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
--- config.color_scheme = "GitHub Dark"
-config.char_select_bg_color = "#282A36"
-config.char_select_fg_color = "#F8F8F2"
+config.color_scheme = "Tokyo Night Moon"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12.0
 config.use_ime = true
@@ -20,26 +18,40 @@ config.scrollback_lines = 100000
 config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
 config.tab_max_width = 100
+config.window_frame = {
+	font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" }),
+	font_size = 16,
+}
 
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local background = "#5c6d74"
-	local foreground = "#FFFFFF"
+	local background = "#222436"
+	local foreground = "#828bb8"
 	local edge_background = "none"
+
 	if tab.is_active then
-		background = "#ae8b2d"
-		foreground = "#FFFFFF"
+		background = "#3b4261"
+		foreground = "#c8d3f5"
+	elseif hover then
+		background = "#2f334d"
+		foreground = "#c8d3f5"
 	end
+
 	local edge_foreground = background
-	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+	local title = " " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. " "
+
 	return {
 		{ Background = { Color = edge_background } },
 		{ Foreground = { Color = edge_foreground } },
 		{ Text = SOLID_LEFT_ARROW },
+
 		{ Background = { Color = background } },
 		{ Foreground = { Color = foreground } },
 		{ Text = title },
+
 		{ Background = { Color = edge_background } },
 		{ Foreground = { Color = edge_foreground } },
 		{ Text = SOLID_RIGHT_ARROW },
