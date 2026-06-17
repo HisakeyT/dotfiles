6f4a54c6bf63c5cb753409a58394c12894b2fb7d
hisakeyT <110322156+HisakeyT@users.noreply.github.com>
2026-06-17 15:52:20 +0900

    feat: add go.lua

---
+++ config/nvim/lua/plugins/go.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              staticcheck = false,
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        go = { "gofmt" },
+      },
+    },
+  },
+}
