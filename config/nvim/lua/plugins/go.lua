9522c7ef68d3e0cf9ae32e15de7bceb181c2117d
hisakeyT <110322156+HisakeyT@users.noreply.github.com>
2026-09-04 21:15:14 +0900

    feat: go.lua の修正

--- config/nvim/lua/plugins/go.lua
+++ config/nvim/lua/plugins/go.lua
@@ -1,6 +1,12 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      -- mise shims を PATH に載せておく（GUI 起動時など shell の mise activate を経由しない場合の対策)
+      vim.env.PATH = vim.fn.expand("~/.local/share/mise/shims") .. ":" .. vim.env.PATH
+      -- リポジトリ側の .go-version が未インストールでも解決できるように固定
+      vim.env.MISE_GO_VERSION = "latest"
+    end,
     opts = {
       servers = {
         gopls = {
